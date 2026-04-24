92785555d5740d5c7c046b5a284664429da16fe0
lpenafielf2-dev <279033461+lpenafielf2-dev@users.noreply.github.com>
2026-04-24 11:10:41 -0500

    cambio 1

--- repo.py
+++ repo.py
@@ -1,1 +1,9 @@
 print("Hello World")
+a=0
+b=0
+
+suma1=int(input("ingresa un numero: "))
+suma2=int(input("ingresa otro numero: "))
+
+resultado= suma1 + suma2
+print("el resultado es: ", resultado)
